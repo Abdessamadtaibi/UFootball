2c21ace395eff9b1956e003231c07fc837b9a922
Abdessamad <taibiabdessamad2003@gmail.com>
2026-04-27 23:15:51 +0100

    fixing the mobile apps version

--- u13_backend/settings.py
+++ u13_backend/settings.py
@@ -30,11 +30,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['www.ufootball.siliad.ma','ufootball.siliad.ma']
-CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     'https://www.ufootball.siliad.ma',
-   'https://ufootball.siliad.ma',
- ]
+    'https://ufootball.siliad.ma',
+  ]
 
 # Application definition
 INSTALLED_APPS = [
